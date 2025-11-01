cfb78e743950b8ea5ecf3740fe6c3e72f9992bd3
Alexsandr Kardash <areswarmoney01@gmail.com>
2025-11-01 11:25:22 +0200

    initial commit

---
+++ js/task-1.js
@@ -0,0 +1,17 @@
+//  Функція перевірки місткості контейнера
+function isEnoughCapacity(products, containerSize) {
+  // Отримуємо масив значень з об’єкта (кількість кожного товару)
+  const values = Object.values(products);
+
+  // Рахуємо загальну кількість товарів
+  const totalProducts = values.reduce((sum, quantity) => sum + quantity, 0);
+
+  // Повертаємо true, якщо товарів менше або дорівнює розміру контейнера
+  return totalProducts <= containerSize;
+}
+
+//  Перевірка роботи функції
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)); // true
+console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)); // false
